5c0732e8dfb299dbb40a3b8694027107885356de
Candy Siyabonga Khumalo <163538990+skhumal@users.noreply.github.com>
2026-01-06 18:17:50 +0200

    Add files via upload

---
+++ FOOD-RESTAURANT/main.js
@@ -0,0 +1,61 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+const videoTrack = document.getElementById("video-track");
+const scrollLeftBtn = document.getElementById("scroll-left");
+const scrollRightBtn = document.getElementById("scroll-right");
+
+// Scroll buttons
+scrollLeftBtn.addEventListener("click", () => {
+  videoTrack.scrollLeft -= 300;
+});
+
+scrollRightBtn.addEventListener("click", () => {
+  videoTrack.scrollLeft += 300;
+});
+
+// Play video on hover
+const videos = videoTrack.querySelectorAll("video");
+
+videos.forEach((video) => {
+  video.addEventListener("mouseenter", () => {
+    video.play();
+  });
+
+  video.addEventListener("mouseleave", () => {
+    video.pause();
+    video.currentTime = 0;
+  });
+});
